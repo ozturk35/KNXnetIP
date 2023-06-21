044209e0d9029b9133cd197972dafb2301a3e28d
ozturk35 <ibrahimozturk35@yandex.com>
2023-06-22 01:55:36 +0300

    Move GPIO port defines to Knx.c

--- main/Include/Port_Cfg.h
+++ main/Include/Port_Cfg.h
@@ -1,14 +1,4 @@
-/* Port Configuration */
 
-#define WS2812_LED   (GPIO_NUM_3)
-
-#define TRSW         (GPIO_NUM_8)
-
-#define RX_TPUART2    (GPIO_NUM_17)
-#define TX_TPUART2    (GPIO_NUM_18)
-
-#define RESET_TPUART (GPIO_NUM_42)
-#define SAVE_TPUART  (GPIO_NUM_41)
 
 /* W5500 SPI-ETH Pins */
 #define ETH_SPI_PHY_RST_GPIO (GPIO_NUM_4)
